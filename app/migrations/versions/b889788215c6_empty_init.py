f27a5ef59fcbab2dbd9f45b6361374a13cb847af
+ <+>
2023-10-01 04:02:32 +0300

    Completed Assignment

---
+++ app/migrations/versions/b889788215c6_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b889788215c6
+Revises: 
+Create Date: 2023-10-01 03:44:07.911196
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b889788215c6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
